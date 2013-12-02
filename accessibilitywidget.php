4c5250631e3de53f8a53b4e20b1a79a1a263d850
webgrrrl <webgrrrl@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-12-02 08:07:08 +0000

    Committing version 1.2 with the following changes: * Added option to customise controller text, i.e. the words you click on to change the text size.
    git-svn-id: https://plugins.svn.wordpress.org/accessibility-widget/tags/1.2@813544 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ accessibilitywidget.php
@@ -0,0 +1,98 @@
+<?php
+/*
+Plugin Name: Accessibility Widget
+Plugin URI: http://webgrrrl.net/archives/accessibility-widget-revived.htm
+Description: Adds a sidebar widget to enlarge text size in your WP site. Originally created by Tane of Digital Spaghetti (http://www.tripcastradio.com/) and revived by Lorna of WebGrrrl.net.
+Author: Lorna Timbah
+Version: 1.2
+Author URI: http://webgrrrl.net
+*/
+class widget_accesstxt extends WP_Widget {
+  /** constructor */
+  function widget_accesstxt() {
+    parent::WP_Widget(false, $name = 'Accessibility Widget');	
+  }
+  /** @see WP_Widget::widget */
+  function widget($args, $instance) {
+    extract( $args );
+    $title = apply_filters('widget_title', $instance['title']);
+    $tags = str_replace(" ", "", $instance['tags']); // remove whitespaces             
+    $fontsize = str_replace(" ", "", $instance['fontsize']); // remove whitespaces
+    $afontsize = explode(",", $fontsize); // transform into arrays
+    $controls = explode(",", str_replace(" ", "", $instance['controls'])); // remove whitespaces then transform into arrays
+    
+    echo $before_widget;
+    if ( $title ) echo $before_title . $title . $after_title;
+    ?>
+    <script type="text/javascript">
+		//Specify affected tags. Add or remove from list
+		var tgs = new Array(<?php echo "'" . str_replace(",", "','", $tags) . "'"; ?>);
+		//Specify spectrum of different font sizes
+		var szs = new Array(<?php echo "'" . str_replace(",", "','", $fontsize) . "'"; ?>);
+		var startSz = 2;
+		function ts( trgt,inc ) {
+			if (!document.getElementById) return
+			var d = document,cEl = null,sz = startSz,i,j,cTags;
+			sz = inc;
+			if ( sz < 0 ) sz = 0;
+			if ( sz > 6 ) sz = 6;
+			startSz = sz;
+			if ( !( cEl = d.getElementById( trgt ) ) ) cEl = d.getElementsByTagName( trgt )[ 0 ];
+			cEl.style.fontSize = szs[ sz ];
+			for ( i = 0 ; i < tgs.length ; i++ ) {
+				cTags = cEl.getElementsByTagName( tgs[ i ] );
+				for ( j = 0 ; j < cTags.length ; j++ ) cTags[ j ].style.fontSize = szs[ sz ];
+			}
+		}
+		</script>
+    <ul>
+      <li><?php
+      foreach ($afontsize as $key => $value) {
+        $controltext = count($controls) < 1 ? $controls[$key] : reset($controls);
+        echo "<a href=\"javascript:ts('body'," . $key . ")\" style=\"font-size:" . $value . "\">" . $controltext . "</a>&nbsp;&nbsp;";
+      }
+      ?></li>
+    </ul>
+    <?php echo $after_widget; ?>
+    <?php
+  }
+  /** @see WP_Widget::update -- do not rename this */
+  function update($new_instance, $old_instance) {		
+		$instance = $old_instance;
+		$instance['title'] = strip_tags($new_instance['title']);
+		$instance['tags'] = strip_tags($new_instance['tags']);
+		$instance['fontsize'] = strip_tags($new_instance['fontsize']);  
+		$instance['controls'] = strip_tags($new_instance['controls']);
+    return $instance;
+  }
+  /** @see WP_Widget::form -- do not rename this */
+  function form($instance) {
+    $title = esc_attr($instance['title']);
+    $tags = ($instance['tags'] == "" ? "body,p,li,td" : esc_attr($instance['tags']));
+    $fontsize = ($instance['fontsize'] == "" ? "90%, 100%, 110%, 120%" : esc_attr($instance['fontsize']));
+    $controls = ($instance['controls'] == "" ? "T" : esc_attr($instance['controls']));
+  ?>
+  <p>
+    <label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:'); ?></label>
+    <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo $title; ?>" />
+  </p>
+  <p>
+    <label for="<?php echo $this->get_field_id('tags'); ?>"><?php _e('Resize the following HTML/CSS tags:'); ?></label>
+    <input class="widefat" id="<?php echo $this->get_field_id('tags'); ?>" name="<?php echo $this->get_field_name('tags'); ?>" type="text" value="<?php echo $tags; ?>" /><br />
+    Separate each tag with a comma (,)
+  </p>
+  <p>
+    <label for="<?php echo $this->get_field_id('fontsize'); ?>"><?php _e('Set to these sizes:'); ?></label>
+    <input class="widefat" id="<?php echo $this->get_field_id('fontsize'); ?>" name="<?php echo $this->get_field_name('fontsize'); ?>" type="text" value="<?php echo $fontsize; ?>" /><br />
+    Separate each size with a comma (,)
+  </p>
+  <p>
+    <label for="<?php echo $this->get_field_id('controls'); ?>"><?php _e('Set controller text:'); ?></label>
+    <input class="widefat" id="<?php echo $this->get_field_id('controls'); ?>" name="<?php echo $this->get_field_name('controls'); ?>" type="text" value="<?php echo $controls; ?>" /><br />
+    Separate each controller text with a comma (,)
+  </p>
+  <?php
+  }
+} // end class widget_accesstxt
+add_action('widgets_init', create_function('', 'return register_widget("widget_accesstxt");'));
+?>
---
+++ readme.txt
@@ -0,0 +1,57 @@
+=== Accessibility Widget ===
+Contributors: webgrrrl
+Tags: accessibility, widget, widgets, formatting, css, style, text, wcag
+Requires at least: 3.0.1
+Tested up to: 3.7.1
+Stable tag: 1.2
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Adds a sidebar widget to change text size in your WP site. Future feature: change colour contrast.
+
+== Installation ==
+
+= Automatic installation =
+1. In your WordPress dashboard, select Plugins / Add.
+1. Search for Accessibility Widget, and select Install.
+1. Go to Appearances / Widgets.
+1. Drag the Accessibility Widget to your widget area and select Save.
+
+= Manual installation =
+1. Upload `accessibilitywidget.php` to the `/wp-content/plugins/` directory.
+1. Activate the plugin through the 'Plugins' menu in WordPress.
+1. Place `<?php do_action('widget_accesstxt'); ?>` in your templates.
+
+== Frequently Asked Questions ==
+
+= How do I use this plugin? =
+
+Follow the Installation instructions. Below are options you can set in the widget:-
+
+*Resize the following HTML/CSS tags:* 
+Specify all the HTML or CSS tags you want to resize in this section. By default, the HTML tag body, p, li and td is set.
+Different WordPress theme may have different and unique stylesheet classes and IDs. You can switch on your browser's built-in Developer Tools and use the Inspect Element option to detect what HTML/CSS codes to use.
+
+*Set to these sizes:*
+This is where you set the font-size you want to enable for the users to use (read up on the CSS font-size property at W3Schools). The simplest font-size group you can use are smaller, inherit, larger.
+
+*Set controller text:*
+By default, the widget displays letter "T" that appears in your widget depending on the number of font sizes you use, that is if you set only two font-size, then only two letter T appears, and so forth. Starting with version 1.2., you can set it to use more meaningful words, like "Small, Normal, Large" for each size you specify.   
+
+= Can I use this on a non-widgetized website / theme? =
+
+Not tested, but theoretically yes, you can. Follow the Manual installation steps in the Installation section for details.
+
+== Changelog ==
+
+= 1.2 =
+* Added option to customise controller text, i.e. the words you click on to change the text size.
+
+= 1.1.1 =
+* Added HTML tags p, li and td options by default for first-time installation.
+
+= 1.1 =
+* Added options into the widget form for easy customisation.
+
+= 1.0 =
+* First release.
